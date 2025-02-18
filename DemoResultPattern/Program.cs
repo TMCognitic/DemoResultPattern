9fc39103de1688b235892d6893d78b93b0866375
Morre Thierry <thierry.morre@cognitic.be>
2025-02-18 10:05:09 +0100

    Ajout rappel du CQS fait le 17/02/2025

--- DemoResultPattern/Program.cs
+++ DemoResultPattern/Program.cs
@@ -21,3 +21,39 @@ foreach(Data data in result.Content)
 {
     Console.WriteLine(data.Value);
 }
+
+
+/// <summary>
+/// Refresh sur le CQS de la correction du 17/02/2025
+/// </summary>
+public interface ICommandDefinition
+{
+}
+
+public interface ICommandHandler<TCommand>
+    where TCommand : ICommandDefinition
+{
+    Result Execute(TCommand command);
+}
+
+public interface ICommandAsyncHandler<TCommand>
+    where TCommand : ICommandDefinition
+{
+    Task<Result> ExecuteAsync(TCommand command);
+}
+
+public interface IQueryDefinition<TResult>
+{
+}
+
+public interface IQueryHandler<TQuery, TResult>
+    where TQuery : IQueryDefinition<TResult>
+{
+    Result<TResult> Execute(TQuery command);
+}
+
+public interface IQueryAsyncHandler<TQuery, TResult>
+    where TQuery : IQueryDefinition<TResult>
+{
+    Task<Result<TResult>> ExecuteAsync(TQuery command);
+}
